38d3b2a964ee59f093d93d6ec2b530364fd2f7c9
Dmytro Shamenko <idetis@gmail.com>
2021-01-18 17:13:44 +0200

    Tests for unquoted type.

--- testdata/inputs.tf
+++ testdata/inputs.tf
@@ -1,24 +1,24 @@
 variable "aws_region" {
-  default     = "us-east-1"
   description = "The AWS region in which all resources will be created."
-  type        = "string"
+  type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_id" {
   description = "The id of the specific VPC to retrieve."
-  type        = "string"
-  default = "1"
+  type        = string
+  default     = "1"
 }
 
 variable "instance_count" {
   description = "The count of desired instances of EC2."
-  type        = "number"
+  type        = number
   default     = "2"
 }
 
 variable "zones" {
-  type = "list"
-  description = "The selected zones"
+  description = "The selected zones for deployment."
+  type        = list(string)
   default = [
     "us-east-1", "us-east-2"
   ]
